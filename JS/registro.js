a871c818ed48801b8812bc346b73e7db4d594662
jucacastro <juanccastro@estudiante.uniajc.edu.co>
2024-10-29 18:23:11 -0500

    agregue mas codigo

--- JS/registro.js
+++ JS/registro.js
@@ -1,44 +1,47 @@
-
-
 document.getElementById("iniciarCompra").addEventListener("click", function(validar) {
     validar.preventDefault();
 
-    const nombreComprador = document.getElementById("nComprador").value;
-    const presupuestoMaximo = document.getElementById("pMaximo").value;
-    const cantidadMaxima = document.getElementById("cMaxima").value;
-    const direccion = document.getElementById("direccion").value;
+    const nombreComprador = document.getElementById("nComprador").value.trim();
+    const presupuestoMaximo = document.getElementById("pMaximo").value.trim();
+    const cantidadMaxima = document.getElementById("cMaxima").value.trim();
+    const direccion = document.getElementById("direccion").value.trim();
     const metodoEntrega = document.querySelector('input[name="estado"]:checked');
-    const entregaSeleccionada = metodoEntrega ? metodoEntrega.value : "";
+    let entregaSeleccionada = "";
+        if (metodoEntrega) {
+         entregaSeleccionada = metodoEntrega.value;
+}
 
-    
-    if (nombreComprador === "" || presupuestoMaximo === "" || cantidadMaxima === "" || direccion === "" || entregaSeleccionada === "") {
+
+    if (!nombreComprador || !presupuestoMaximo || !cantidadMaxima || !direccion || !entregaSeleccionada) {
         alert("Por favor, completa todos los campos y selecciona un método de entrega.");
         return;
     }
-    
+
+   
     if (nombreComprador.length > 20) {
         alert("El nombre debe ser solo texto y no exceder los 20 caracteres.");
         return;
     }
 
-    if (presupuestoMaximo < 0) {
+
+    if (isNaN(presupuestoMaximo) || presupuestoMaximo <= 0) {
         alert("El presupuesto máximo debe ser un número positivo en pesos.");
         return;
     }
 
-    if (cantidadMaxima > 20) {
+    
+    if (isNaN(cantidadMaxima) || cantidadMaxima <= 0 || cantidadMaxima > 20) {
         alert("La cantidad máxima de productos debe ser un número positivo y no exceder los 20 productos.");
         return;
     }
 
-    
     localStorage.setItem("nombreComprador", nombreComprador);
     localStorage.setItem("presupuestoMaximo", `$${parseInt(presupuestoMaximo).toLocaleString('es-CO')}`);
     localStorage.setItem("cantidadMaxima", cantidadMaxima);
     localStorage.setItem("direccion", direccion);
     localStorage.setItem("entregaSeleccionada", entregaSeleccionada);
 
-    // Redirige a la página de productos
+   
     window.location.href = "../HTML/listadoProductos.html";
 });
 
